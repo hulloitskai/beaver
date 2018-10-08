33b83333a8e6fcb4394a938cd94b5574ef0eb61b
Steven Xie <dev@stevenxie.me>
2018-10-08 08:09:24 -0400

    Initial commit, bootstrapped with Vue UI

---
+++ core/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <p>It loads!</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+body {
+  margin: 0;
+
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
